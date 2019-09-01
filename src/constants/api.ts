b22d04f60aa0607713da79fa3c12d7570791e1d9
madongzhe <490854764@qq.com>
2019-09-01 16:02:51 +0800

    分类

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,4 @@
 export const host = HOST;
-export const host_img = HOST_IMG;
 
 // home
 export const API_HOME = `${host}/api/home`
@@ -7,6 +6,19 @@ export const API_HOME_RECOMMEND = `${host}/api/goodsList`
 
 // item
 export const API_ITEM = `${host}/api/goodsItem`
+
+// cate
+export const API_CATE = `${host}/xhr/list/category.json`
+export const API_CATE_SUB = `${host}/xhr/list/subCate.json`
+export const API_CATE_SUB_LIST = `${host}/xhr/list/l2Items2.json`
+
+// cart
+export const API_CART = `${host}/xhr/promotionCart/getCarts.json`
+export const API_CART_NUM = `${host}/xhr/promotionCart/getMiniCartNum.json`
+export const API_CART_RECOMMEND = `${host}/xhr/rcmd/cart.json`
+export const API_CART_ADD = `${host}/xhr/promotionCart/add.json`
+export const API_CART_UPDATE = `${host}/xhr/promotionCart/update.json`
+export const API_CART_UPDATE_CHECK = `${host}/xhr/promotionCart/updateCheck.json`
 
 // user
 export const API_USER = `${host}/xhr/user/getDetail.json`
--- src/pages/cate/index.tsx
+++ src/pages/cate/index.tsx
@@ -1,16 +1,88 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import Taro, { Component } from '@tarojs/taro'
+import { View, ScrollView } from '@tarojs/components'
+import { Loading } from '@components'
+import { connect } from '@tarojs/redux'
+import * as actions from '@actions/cate'
+import { getWindowHeight } from '@utils/style'
+import Menu from './menu'
+import List from './list'
+import Banner from './banner'
+import './index.scss'
 
-export default class CateIndex extends Component {
-  config: Config = {
+interface IAppProps{
+  dispatchMenu:any,
+  menu:any,
+  category:Array<any>,
+}
+@connect(state => state.cate, { ...actions })
+class Cate extends Component<IAppProps> {
+  config = {
     navigationBarTitleText: '分类'
   }
 
+  state = {
+    current: -1,
+    loaded: false,
+    loading: false
+  }
+
+  componentDidMount() {
+    this.props.dispatchMenu().then((res) => {
+      this.setState({
+        loaded: true,
+        current: res.categoryList[0].id
+      })
+    })
+  }
+
+  handleMenu = (id) => {
+    this.setState({ loading: true }, () => {
+      this.setState({ current: id, loading: false })
+    })
+  }
+
   render () {
+    const { menu, category } = this.props
+    const { current, loading } = this.state
+    const currentCategory = category.find(item => item.id === current) || {}
+    const banner = currentCategory.focusBannerList || []
+    const list = currentCategory.categoryGroupList || []
+    const height = getWindowHeight()
+
+    if (!this.state.loaded) {
+      return <Loading />
+    }
+
     return (
-      <View className='index'>
-        <Text>分类</Text>
+      <View className='cate'>
+        <ScrollView
+          scrollY
+          className='cate__menu'
+          style={{ height }}
+        >
+          <Menu
+            current={current}
+            list={menu}
+            onClick={this.handleMenu}
+          />
+        </ScrollView>
+        {/* 通过切换元素实现重置 ScrollView 的 scrollTop */}
+        {loading ?
+          <View /> :
+          <ScrollView
+            scrollY
+            className='cate__list'
+            style={{ height }}
+          >
+            <View className='cate__list-wrap'>
+              <Banner banner={banner} />
+              <List list={list} />
+            </View>
+          </ScrollView>
+        }
       </View>
     )
   }
 }
+
+export default Cate
